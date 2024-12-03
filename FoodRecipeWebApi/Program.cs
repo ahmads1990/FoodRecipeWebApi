729467ca81bb887d45e2ab93ff5c416b2fc4e128
Ahmad Khaled <Ahmads20002000@gmail.com>
2024-12-03 20:02:32 +0200

    Configure entityframework

--- FoodRecipeWebApi/Program.cs
+++ FoodRecipeWebApi/Program.cs
@@ -1,11 +1,25 @@
+using FoodRecipeWebApi.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Diagnostics;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Database
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options
+        .UseSqlServer(connectionString)
+        .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking)
+        .LogTo(log => Debug.WriteLine(log), LogLevel.Information)
+        .EnableSensitiveDataLogging();
+});
 
 var app = builder.Build();
 
