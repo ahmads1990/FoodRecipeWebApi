536c8c520ff063fbdf0a796b3095ebb6015b4db3
ahmads1990 <ahmads20002000@gmail.com>
2024-12-09 05:20:11 +0200

    Setup Autofac

---
+++ FoodRecipeWebApi/Config/AutofacModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using FoodRecipeWebApi.Data.Repo;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodRecipeWebApi.Config;
+
+public class AutofacModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<DbContext>().InstancePerLifetimeScope();
+
+        builder.RegisterGeneric(typeof(Repository<>)).As(typeof(IRepository<>)).InstancePerLifetimeScope();
+
+        builder.RegisterAssemblyTypes(typeof(Program).Assembly)
+            .Where(c => c.Name.EndsWith("Service"))
+            .AsImplementedInterfaces()
+            .InstancePerLifetimeScope();
+    }
+}
